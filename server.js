6f37378e8caa5d2acf1debb5c79994dcbeea1f55
KaineOfficial <contact@hostynet.fr>
2026-04-09 19:47:51 +0200

    Correction CSS

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ const port = process.env.PORT || 3000;
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+// servir les fichiers statiques depuis public/
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
